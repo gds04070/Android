8370436cf9f35714c139a91c59a59a336bf62eea
lumiere <heesung6701@naver.com>
2018-12-23 19:59:04 +0900

    2018/12/23 add CalendarViewHolder

---
+++ app/src/main/java/concertrip/sopt/com/concertrip/activities/main/fragment/calendar/viewholder/CalendarViewHolder.kt
@@ -0,0 +1,8 @@
+package concertrip.sopt.com.concertrip.activities.main.fragment.calendar.viewholder
+
+import android.widget.TextView
+import kotlin.properties.Delegates
+
+class CalendarViewHolder {
+     var  tvItemGridView : TextView by Delegates.notNull()
+}
