3b424f37be9d8b0968984483ebed9406b1016bb0
lumiere <heesung6701@naver.com>
2018-12-23 20:04:42 +0900

    2018/12/23 add class model

--- app/src/main/java/concertrip/sopt/com/concertrip/activities/main/fragment/mypage/ticket/TicketFragment.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/activities/main/fragment/mypage/ticket/TicketFragment.kt
@@ -1,4 +1,4 @@
-package concertrip.sopt.com.concertrip.activities.main.fragment.ticket
+package concertrip.sopt.com.concertrip.activities.main.fragment.mypage.ticket
 
 import android.content.Context
 import android.net.Uri
--- app/src/main/java/concertrip/sopt/com/concertrip/model/Artist.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/model/Artist.kt
@@ -1,4 +1,10 @@
 package concertrip.sopt.com.concertrip.model
 
 class Artist {
+    var idx : Int = 0
+    var profileImg : String =""
+    var backgoundImg : String =""
+    var name : String = ""
+    var genre : String = ""
+    var youtubeUrl : String = ""
 }
