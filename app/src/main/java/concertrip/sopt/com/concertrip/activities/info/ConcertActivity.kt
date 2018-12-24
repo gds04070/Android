830c9a25a089491d9376cc656026de2e2f74a748
lumiere <heesung6701@naver.com>
2018-12-24 20:52:47 +0900

    Revert "2014/12/24 fragment 세팅"
    This reverts commit 4be3b942e07b930315780185b3ffef6cc8e6aa92.

--- app/src/main/java/concertrip/sopt/com/concertrip/activities/info/ConcertActivity.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/activities/info/ConcertActivity.kt
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import concertrip.sopt.com.concertrip.R
 import concertrip.sopt.com.concertrip.list.adapter.ArtistThumbListAdapter
 import concertrip.sopt.com.concertrip.model.Artist
-import concertrip.sopt.com.concertrip.model.Concert
 
 import kotlinx.android.synthetic.main.content_concert.*
 
