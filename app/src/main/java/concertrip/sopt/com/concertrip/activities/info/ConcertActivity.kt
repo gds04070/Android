67b096528e68c1014a1a5dcfebc97953320f5472
cchloe2311 <cchloe2311@daum.net>
2018-12-24 17:22:29 +0900

    2018/12/24 fixed manifest

--- app/src/main/java/concertrip/sopt/com/concertrip/activities/info/ConcertActivity.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/activities/info/ConcertActivity.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import concertrip.sopt.com.concertrip.R
+import concertrip.sopt.com.concertrip.list.adapter.ArtistThumbListAdapter
+import concertrip.sopt.com.concertrip.model.Artist
+import concertrip.sopt.com.concertrip.model.Concert
 
 import kotlinx.android.synthetic.main.activity_concert.*
+import kotlinx.android.synthetic.main.content_concert.*
 
 class ConcertActivity : AppCompatActivity() {
+
+    //var dataList = arrayListOf<Artist>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_concert)
 //        setSupportActionBar(toolbar)
 
+        val mAdapter = ArtistThumbListAdapter(this, Artist.getDummyArray())
+        recycler_view.adapter = mAdapter
     }
 
 }
--- app/src/main/java/concertrip/sopt/com/concertrip/list/adapter/ArtistThumbListAdapter.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/list/adapter/ArtistThumbListAdapter.kt
@@ -1,6 +1,7 @@
 package concertrip.sopt.com.concertrip.list.adapter
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -11,6 +12,12 @@ import concertrip.sopt.com.concertrip.R.id.parent
 import concertrip.sopt.com.concertrip.list.viewholder.ArtistThumbViewHolder
 import concertrip.sopt.com.concertrip.list.viewholder.TicketViewHolder
 import concertrip.sopt.com.concertrip.model.Artist
+import android.util.Log
+import android.view.View
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class ArtistThumbListAdapter(var mContext : Context, var dataList: ArrayList<Artist>) : RecyclerView.Adapter<ArtistThumbViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): ArtistThumbViewHolder {
@@ -21,7 +28,29 @@ class ArtistThumbListAdapter(var mContext : Context, var dataList: ArrayList<Art
     override fun getItemCount(): Int = dataList!!.size
 
     override fun onBindViewHolder(holder: ArtistThumbViewHolder, position: Int) {
-        Glide.with(mContext).load(dataList!!.get(position).profileImg).apply(RequestOptions.circleCropTransform()).into(holder!!.ivArtist)
+        //Glide.with(mContext).load(dataList[position].profileImg).apply(RequestOptions.circleCropTransform()).into(holder!!.ivArtist)
+        Glide.with(mContext).load(dataList[position].profileImg).listener(object : RequestListener<Drawable> {
+            override fun onLoadFailed(
+                e: GlideException?,
+                model: Any?,
+                target: Target<Drawable>?,
+                isFirstResource: Boolean
+            ): Boolean {
+                return true
+            }
+
+            override fun onResourceReady(
+                resource: Drawable?,
+                model: Any?,
+                target: Target<Drawable>?,
+                dataSource: DataSource?,
+                isFirstResource: Boolean
+            ): Boolean {
+                return false
+            }
+
+        }).apply(RequestOptions().fitCenter()).into(holder.ivArtist)
+        Log.d("1223", "posiiton : "+ dataList.get(position) + dataList!!.get(position).profileImg) //.apply(RequestOptions.circleCropTransform())
         holder!!.tvArtist.setText(dataList!!.get(position).name)
         holder.ivArtist.setOnClickListener{
             /*TODO
