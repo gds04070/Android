5cf7632535d05ebaa82b51a20ffb1ef4765a6db6
gds04070 <gds04070@gmail.com>
2018-12-26 01:35:25 +0900

    2018/12/26 add AlarmListAdapter+Setting fragment

---
+++ app/src/main/java/concertrip/sopt/com/concertrip/list/adapter/AlarmListAdapter.kt
@@ -0,0 +1,25 @@
+package concertrip.sopt.com.concertrip.list.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import concertrip.sopt.com.concertrip.R
+import concertrip.sopt.com.concertrip.list.viewholder.AlarmViewHolder
+import concertrip.sopt.com.concertrip.model.Alarm
+
+
+class AlarmListAdapter(var mContext: Context, var dataList : ArrayList<Alarm>) : RecyclerView.Adapter<AlarmViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlarmViewHolder {
+        var view : View = LayoutInflater.from(mContext).inflate(R.layout.li_alarm, parent, false )
+        return AlarmViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = dataList.size
+
+    override fun onBindViewHolder(holder: AlarmViewHolder, position: Int) {
+        holder.tvdate.text = dataList[position].title
+        holder.tvtitle.text = dataList[position].date
+    }
+}
--- app/src/main/java/concertrip/sopt/com/concertrip/model/Alarm.kt
+++ app/src/main/java/concertrip/sopt/com/concertrip/model/Alarm.kt
@@ -1,4 +1,6 @@
 package concertrip.sopt.com.concertrip.model
 
 class Alarm {
+    var title : String = ""
+    var date : String = ""
 }
