fee47fcbe8afb87905cf3fb59273fc8eb1b9bcd9
gds04070 <gds04070@gmail.com>
2018-12-25 18:15:15 +0900

    2018/12/25 add HorizontalList

---
+++ app/src/main/java/concertrip/sopt/com/concertrip/list/adapter/HorizontalListAdapter.kt
@@ -0,0 +1,25 @@
+package concertrip.sopt.com.concertrip.list.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import concertrip.sopt.com.concertrip.R
+import concertrip.sopt.com.concertrip.list.viewholder.HorizontalVIewHolder
+
+
+class HorizontalListAdapter(val mContext : Context, var dataList: ArrayList<String>) : RecyclerView.Adapter<HorizontalVIewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): HorizontalVIewHolder {
+        var view : View = LayoutInflater.from(mContext).inflate(R.layout.li_tag, parent, false)
+        return HorizontalVIewHolder(view)
+
+    }
+
+    override fun getItemCount(): Int = dataList.size
+
+    override fun onBindViewHolder(holder: HorizontalVIewHolder, position: Int) {
+       holder.tvtext.text = dataList[position]
+    }
+}
---
+++ app/src/main/java/concertrip/sopt/com/concertrip/list/viewholder/HorizontalVIewHolder.kt
@@ -0,0 +1,11 @@
+package concertrip.sopt.com.concertrip.list.viewholder
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.TextView
+import concertrip.sopt.com.concertrip.R
+
+
+class HorizontalVIewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+    val tvtext : TextView = itemView.findViewById(R.id.tv_text) as TextView
+}
